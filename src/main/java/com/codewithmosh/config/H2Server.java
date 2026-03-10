b53509289a1fe575573f2b0ea69b78bc83f3b2e5
scarletabreu <sxas0002@ce.pucmm.edu.do>
2026-03-09 21:28:12 -0400

    Fix H2 embedded for Azure

--- src/main/java/com/codewithmosh/config/H2Server.java
+++ src/main/java/com/codewithmosh/config/H2Server.java
@@ -1,26 +1,20 @@
 package com.codewithmosh.config;
 
-import org.h2.tools.Server;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
-import java.util.concurrent.ExecutionException;
 
-//Clase para inicializar h2 de manera automatica, siempre que se ejecute la aplicacion.
 public class H2Server {
-    public static void start(){
-        try{
-            Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
-            System.out.print("H2 iniciado");
-            try {
-                Connection conn = DriverManager.getConnection("jdbc:h2:~/parcial2", "sa", "sa");
-            } catch (Exception e){
-                System.out.print("No se creo el server.");
-            }
-        }catch (Exception e){
+
+    public static void start() {
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:h2:~/parcial2",
+                "sa",
+                "sa")) {
+
+            System.out.println("H2 embebido iniciado");
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 }
